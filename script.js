6aec34d8870cea9edaf608ae52d42d1d72bba617
gautam-dawar-99 <gautamdawar0276@gmail.com>
2024-01-27 11:25:01 +0530

    change added

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+function age(age) {
+    if (age >= 18) {
+        document.write("You can Vote!!")
+    }
+    else{
+        document.write("You can't Vote!!")
+    }
+}
+var x = prompt("Please enter your age");
+age(x)
+
